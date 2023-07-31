e0c9c613c4e1ed986799e3f6f159ec6da36702ee
Ak_D7 <akshitadeka7@gmail.com>
2023-07-31 20:24:04 +0530

    home initialized

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,33 @@
 import "../Styles/Home.css"
+import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom'
+
 const Home = () => {
+
+  const texts = [" Hired", " Recognised", " Experience"];
+  const [currentTextIndex, setCurrentTextIndex] = useState(0);
+  const [currentText, setCurrentText] = useState(texts[currentTextIndex]);
+
+  useEffect(() => {
+    // Function to update the text every 3 seconds
+    const interval = setInterval(() => {
+      setCurrentTextIndex((prevIndex) => (prevIndex + 1) % texts.length);
+    }, 2000);
+
+    // Clean up the interval when the component unmounts
+    return () => clearInterval(interval);
+  }, [texts.length]);
+
+  useEffect(() => {
+    // Update the currentText whenever currentTextIndex changes
+    setCurrentText(texts[currentTextIndex]);
+  }, [currentTextIndex, texts]);
+
   return (
       <div className="home">
           <div className="intro">
               <h1 style={{color: "antiquewhite"}}>Work Now</h1>
-              <h1 style={{color: "antiquewhite"}}><span style={{color: "#95af29"}}>Get</span> Hired</h1> 
+              <h1 style={{color: "antiquewhite"}}><span style={{color: "#95af29"}}>Get</span>{currentText}</h1> 
               <h1 style={{color: "antiquewhite"}}>Make A Change</h1>
               <br/>
               <h2 style={{color: "#95af29"}}>Connecting Talent with Opportunity:</h2> 
@@ -14,7 +36,7 @@ const Home = () => {
               <Link to={"/getstarted"}><button className="btn">Get Started ↪</button></Link>
           </div>
           <div className="image">
-            <img className="i_m" src="https://media.istockphoto.com/id/1392122525/vector/career-development-concept-modern-vector-illustration-with-people-team-working-together-for.jpg?s=170667a&w=0&k=20&c=JuqDrVhGzE5CA8LiqUDz8PCYyuTdrtRSHD6OJARO_uM=" alt="image"/>
+            <img className="i_m" src="src\assets\intro-img.png" alt="image"/>
           </div>
       </div>
   )
