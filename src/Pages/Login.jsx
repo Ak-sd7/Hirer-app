0cb21e07aada3097b5a0601f79e09dadfb724303
Ak_D7 <akshitadeka7@gmail.com>
2023-08-06 01:31:01 +0530

    login page

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,16 +1,28 @@
+import { Stack } from "@mui/material"
+import "../Styles/login.css"
+import { useState } from "react"
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const submitHandler = (e)=>{
+    e.preventDefault();
+    return( 
+      console.log("submitted")
+    );
+  }
+
   return (
-    <div>
-        <div>
-          <h2 className="lI">LogIn</h2>
-          <form>
-            <input></input>
-            <input></input>
-            <input></input>
-          </form>
-        </div>
-    </div>
+      <div style={{ display: "flex", justifyContent: "center", alignItems: "center", paddingTop: "100px", alignContent:"center"}}>
+        <form onSubmit={submitHandler}>
+          <Stack spacing={5} paddingLeft={5} sx={{ alignItems: "center", justifyContent: "center" }}>
+            <input placeholder="email" type="email" onChange={(e)=>{setEmail(e.target.value)}} value={email} required/>
+            <input placeholder="password" type="password" onChange={(e)=>{setPassword(e.target.value)}} value={password} required/>
+            <button className="LoginB" style={{width: "300px"}}>submit</button>
+          </Stack>
+        </form>
+      </div>
   )
 }
 
