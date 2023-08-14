a65664e60a83cc3ae405e2119dfb531db8ababf2
Ak_D7 <akshitadeka7@gmail.com>
2023-08-14 18:13:38 +0530

    toggle updated

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -2,6 +2,8 @@ import { Stack } from "@mui/material"
 import "../Styles/login.css"
 import { useState } from "react"
 import TStyle from "../subComponents/TStyle"
+import { Link } from "react-router-dom"
+import { toast } from "react-hot-toast";
 
 const Login = () => {
   
@@ -11,26 +13,26 @@ const Login = () => {
   const submitHandler = (e)=>{
     e.preventDefault();
     return(  
-      console.log("submitted")
+      toast.success("Submitted successfully")
     ); 
   }
 
   return (
-      <div style={{ display: "flex", justifyContent: "center", alignItems: "center", paddingTop: "100px", alignContent:"center", flexDirection:"column", maxHeight:"100vh"}}>
+      <div className="lmain">
         <form onSubmit={submitHandler}>
           <Stack spacing={5} paddingLeft={5} sx={{ alignItems: "center", justifyContent: "center" }}>
             <input className="in" placeholder="email" type="email" onChange={(e)=>{setEmail(e.target.value)}} value={email} required/>
             <input className="in" placeholder="password" type="password" onChange={(e)=>{setPassword(e.target.value)}} value={password} required/>
             <div className="toggle-container">
-              <h2>Are you here to hire ?</h2>
+              <h2>Are you here to<span style={{color:"antiquewhite"}}> hire ?</span></h2>
               <TStyle className="toggle" color="warning"/>
             </div>
             <button className="LoginB" style={{width: "300px"}}>submit</button>
           </Stack>
-        </form>
+        </form> 
         <div className="belowor">
-          <h4 style={{paddingRight:"26px", color:"#95af29"}}>OR</h4>
-          <h3 style={{paddingTop: "10px"}}>Register</h3>
+          <h4 style={{color:"#95af29"}}>OR</h4>
+          <Link to={"/register"} className="ll"><h3>Register</h3></Link>
         </div>
       </div>
   )
--- src/subComponents/TStyle.jsx
+++ src/subComponents/TStyle.jsx
@@ -3,7 +3,7 @@ import { styled } from '@mui/material/styles';
 
 const TStyle = styled(Switch)(() => ({
     '& .MuiSwitch-switchBase:not(.Mui-checked)': {
-        color: "#721304", // Color for the unchecked switch    
+        color: "white", // Color for the unchecked switch    
     },
     '& .MuiSwitch-switchBase.Mui-checked': {
       color: "#95af29",
@@ -12,7 +12,7 @@ const TStyle = styled(Switch)(() => ({
       backgroundColor: "#95af29",
     },
     '& .MuiSwitch-track': {
-        backgroundColor: "#AA4A36", // Background color for the unchecked switch track
+        backgroundColor: "white", // Background color for the unchecked switch track
       },
   }));
 
