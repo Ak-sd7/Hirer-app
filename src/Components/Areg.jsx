aa388d86f341ffb31bd316f271daf6327db664bd
Ak_D7 <akshitadeka7@gmail.com>
2023-08-30 04:40:53 +0530

    reg s1

--- src/Components/Areg.jsx
+++ src/Components/Areg.jsx
@@ -1,8 +1,60 @@
+import {useState}  from "react";
+import TStyle from "../subComponents/TStyle"
+import { Stack } from "@mui/material";
+import { toast } from "react-hot-toast";
+import Mreg from "./Mreg";
+const Areg = () => {
 
-const areg = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [uni, setUni] = useState("");
+  const [phoneno, setNumber] = useState("");
+  const [uniGpa, setUniGpa] = useState();
+  const [duration, setDuration] = useState();
+
+  const [showMreg, setShowMreg] = useState(false);
+
+
+  const submitHandler = (e)=>{
+    e.preventDefault();
+    return(  
+      toast.success("Submitted successfully")
+    ); 
+  }
+
+
   return (
-    <div>areg</div>
+    <div>
+      {showMreg ? (
+        <Mreg />
+        ) : (
+        <>
+      <div className="st">
+        <h1>For Jobseekers : </h1>
+        <div className="togg">
+            <h3>Are you here to <span style={{color:"antiquewhite"}}> Hire ?</span></h3>
+            <TStyle className="toggle" size="small" onClick={()=>setShowMreg(!showMreg)}/>
+        </div>
+      </div>
+      <div className="ff">
+        <form onSubmit={submitHandler}>
+          <Stack spacing={5} paddingLeft={5} sx={{ alignItems: "center", justifyContent: "center" }}>
+
+            <input className="in" placeholder="name" type="text" onChange={(e)=>{setName(e.target.value)}} value={name} required/>
+            <input className="in" placeholder="email" type="email" onChange={(e)=>{setEmail(e.target.value)}} value={email} required/>
+            <input className="in" placeholder="password" type="password" onChange={(e)=>{setPassword(e.target.value)}} value={password} required/>
+            <input className="in" placeholder="Phone No." type="tel" pattern="[0-9]{10}" onChange={(e)=>{setNumber(e.target.value)}} value={phoneno} required/>
+            <input className="in" placeholder="University Name" type="text" onChange={(e)=>{setUni(e.target.value)}} value={uni} required/>
+            <input className="in" placeholder="University GPA ( /10 )" type="decimal" onChange={(e)=>{setUniGpa(e.target.value)}} value={uniGpa} required/>
+            <input className="in" placeholder="Duration" type="number" onChange={(e)=>{setDuration(e.target.value)}} value={duration} required/>            
+            <button className="LoginB" style={{width: "230px"}}>submit</button>
+          </Stack>
+        </form>
+      </div>
+      </>)}
+    </div>
   )
 }
 
-export default areg
+export default Areg;
--- src/Components/Mreg.jsx
+++ src/Components/Mreg.jsx
@@ -2,6 +2,8 @@ import { Stack } from "@mui/material"
 import { useState } from "react";
 import TStyle from "../subComponents/TStyle"
 import { toast } from "react-hot-toast";
+import Areg from "./Areg"
+import "../Styles/reg.css";
 
 const Mreg = () => {
 
@@ -13,6 +15,9 @@ const Mreg = () => {
   const [stipend, setStipend] = useState();
   const [duration, setDuration] = useState();
 
+  const [showAreg, setShowAreg] = useState(false);
+
+
   const submitHandler = (e)=>{
     e.preventDefault();
     return(  
@@ -22,27 +27,33 @@ const Mreg = () => {
 
   return (
     <div>
+      {showAreg ? (
+        <Areg />
+        ) : (
+        <>
       <div className="st">
-        <h1>For Hirers</h1>
-        <div className="toggle-container">
-            <h2>Are you a <span style={{color:"antiquewhite"}}> Jobseeker ?</span></h2>
-            <TStyle className="toggle"/>
+        <h1>For Hirers : </h1>
+        <div className="togg">
+            <h3>Are you a <span style={{color:"antiquewhite"}}> Jobseeker ?</span></h3>
+            <TStyle className="toggle" size="small" onClick={()=>setShowAreg(!showAreg)}/>
         </div>
       </div>
-      <div>
+      <div className="ff">
         <form onSubmit={submitHandler}>
           <Stack spacing={5} paddingLeft={5} sx={{ alignItems: "center", justifyContent: "center" }}>
-            <input className="in" placeholder="name" type="text" onChange={(e)=>{setName(e.target.value)}} value={name} required/>
+          <input className="in" placeholder="name" type="text" onChange={(e)=>{setName(e.target.value)}} value={name} required/>
             <input className="in" placeholder="email" type="email" onChange={(e)=>{setEmail(e.target.value)}} value={email} required/>
             <input className="in" placeholder="password" type="password" onChange={(e)=>{setPassword(e.target.value)}} value={password} required/>
+            <input className="in" placeholder="Phone No." type="tel" pattern="[0-9]{10}" onChange={(e)=>{setNumber(e.target.value)}} value={phoneno} required/>
             <input className="in" placeholder="company Name" type="text" onChange={(e)=>{setCompany(e.target.value)}} value={company} required/>
-            <input className="in" placeholder="Phone No. - would not be shared" type="tel" pattern="[0-9]{10}" onChange={(e)=>{setNumber(e.target.value)}} value={phoneno} required/>
             <input className="in" placeholder="Stipend / Year" type="number" onChange={(e)=>{setStipend(e.target.value)}} value={stipend} required/>
             <input className="in" placeholder="Duration" type="number" onChange={(e)=>{setDuration(e.target.value)}} value={duration} required/>            
             <button className="LoginB" style={{width: "230px"}}>submit</button>
           </Stack>
         </form>
       </div>
+      </>)
+    }
     </div>
   )
 }
