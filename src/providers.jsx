f45c6be7d43ab97f0ca2a0bd22983f8f88c464a2
Ak_D7 <akshitadeka7@gmail.com>
2025-03-09 23:59:11 +0530

    providers

---
+++ src/providers.jsx
@@ -0,0 +1,36 @@
+import { createContext, useContext, useState } from "react";
+
+const Context = createContext({isAuthenticated: false});
+
+// eslint-disable-next-line react/prop-types
+export const Provider = ({children})=>{
+    const [isAuthenticated, setIsAuthenticated] = useState(false)
+    const [loading, setLoading] = useState(false);
+    const[user, setUser] = useState({});
+
+    return(
+        <Context.Provider
+            value={{
+                isAuthenticated,
+                setIsAuthenticated,
+                loading,
+                setLoading,
+                user,
+                setUser
+            }}
+        >
+            {children}
+        </Context.Provider>
+    )
+}
+
+// eslint-disable-next-line react-refresh/only-export-components
+export const useContextProvider = ()=>{
+    const context = useContext(Context);
+    if(!context){
+        throw new Error('useContextProvider must be used within Provider')
+    }
+    return context;
+}
+
+export default Context;
